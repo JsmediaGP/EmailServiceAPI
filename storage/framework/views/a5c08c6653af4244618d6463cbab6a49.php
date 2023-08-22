37c33b60a8c4b07af9dc1af4122c53eb85c07df1
JsmediaGP <jgraphics73@gmail.com>
2023-08-22 15:46:23 +0100

    deleted:    app/Models/User.php 	deleted:    bootstrap/cache/.gitignore 	new file:   bootstrap/cache/packages.php 	new file:   bootstrap/cache/services.php 	deleted:    database/.gitignore 	deleted:    database/factories/UserFactory.php 	deleted:    database/migrations/2014_10_12_000000_create_users_table.php 	deleted:    database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php 	deleted:    database/migrations/2019_08_19_000000_create_failed_jobs_table.php 	deleted:    database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php 	deleted:    database/seeders/DatabaseSeeder.php 	deleted:    storage/app/.gitignore 	deleted:    storage/app/public/.gitignore 	deleted:    storage/framework/.gitignore 	deleted:    storage/framework/cache/.gitignore 	deleted:    storage/framework/cache/data/.gitignore 	new file:   storage/framework/cache/data/53/15/5315750cbc5d139969f095139e3809958cddcf8e 	new file:   storage/framework/cache/data/c0/71/c071422490d526a8aada046adf3a9c9fa0593468 	new file:   storage/framework/cache/data/e6/ec/e6ec601a5df6bb90fcdd425d28f28a60fcc2bb2c 	new file:   storage/framework/cache/data/ec/c4/ecc49f49f4da6b940dcde13f0571e79c299871e6 	deleted:    storage/framework/sessions/.gitignore 	new file:   storage/framework/sessions/PuuvB4oFlZGrHrQobfSLCInsEs5b1mnvHcZwQisS 	deleted:    storage/framework/testing/.gitignore 	deleted:    storage/framework/views/.gitignore 	new file:   storage/framework/views/19d1ca22cd8db231f88e0685e9c3a20e.php 	new file:   storage/framework/views/275c7c02e2528e6029079c885e2d2418.php 	new file:   storage/framework/views/5ce09783ade690bd80ee75004925bfe3.php 	new file:   storage/framework/views/6d6c2814442bfb593064cd4df16b5a02.php 	new file:   storage/framework/views/a5c08c6653af4244618d6463cbab6a49.php 	new file:   storage/framework/views/bc3f2791ddfc262969033db2e94f65fd.php 	new file:   storage/framework/views/bdc703ba3272cb94ac9af89962c6d046.php 	new file:   storage/framework/views/c4e0b9f375b2b84345623b105b7dc3a9.php 	new file:   storage/framework/views/dd310000961f2d208873a737c27d849a.php 	new file:   storage/framework/views/e1654b0fccc159b3bbec4bdc5b7b3984.php 	deleted:    storage/logs/.gitignore 	new file:   storage/logs/laravel.log

---
+++ storage/framework/views/a5c08c6653af4244618d6463cbab6a49.php
@@ -0,0 +1,4 @@
+<p>
+    <?php echo e($content); ?>
+
+</p><?php /**PATH /var/www/html/resources/views/email/customMail.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bdc703ba3272cb94ac9af89962c6d046.php
@@ -0,0 +1,4 @@
+<p>
+    <?php echo e($content); ?>
+
+</p><?php /**PATH C:\Users\Js'Media\Desktop\laravel\EmailServiceAPI\resources\views/email/customMail.blade.php ENDPATH**/ ?>
